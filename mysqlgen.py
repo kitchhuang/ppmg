b778d1fa56ea361bd810c8373db1e22134fce3bc
Kitch <kitchhuang@gmail.com>
2016-07-13 16:46:28 +0800

    Create

---
+++ mysqlgen.py
@@ -0,0 +1,34 @@
+from model_generator_pyramid import BaseModelGenerator, Params
+
+params = Params()
+params.dialect = 'mysql'
+params.user = 'roboshopuser'
+params.password = 'howcute121'
+params.host = '192.168.1.55'
+params.db_name = 'roboshop'
+
+params.user = 'root'
+params.password = 'howcute121'
+params.host = '192.168.2.53'
+params.db_name = 'mm'
+
+# params.user = 'root'
+# params.password = 'mysql56forkitch'
+# params.host = '127.0.0.1'
+# params.db_name = 'hangbiaodeng'
+
+
+#schema for db / could be None for default
+params.schema = None
+
+#if true Raise exception if a PK is not present in the table
+params.force_pk = False
+
+#specify the tables you want to generate the model for.
+params.table_list = params.fetch_all_tables_pg(params)
+params.table_list = ["client_kiosk_audio"]
+#params.metadata_name = 'ev5_metadata'
+
+bmg = BaseModelGenerator(params)
+
+bmg.generate_model()
